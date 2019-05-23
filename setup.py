eb0735a520cff5cda815eb83ae1c6df57d345ae1
victorruelle <victorruelle1@hotmail.com>
2019-05-23 10:41:03 +0800

    Restructuring for packaging

--- setup.py
+++ setup.py
@@ -1,34 +1,29 @@
 import setuptools
 
-
-def readme():
-    with open('README.md','r') as f:
-        return f.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
-     name='ExperimentManager',  
-     version='0.1',
-     scripts=['ExperimentManager'] ,
-     author="Victor Ruelle",
-     author_email="ruelle.victor@gmail.com",
-     description="A thoughtless and all-round experiment manager for python",
-     long_description=readme(),
-     license='MIT',
-     install_requires=[
-          'numpy',
-	  'tensorflow',
-	  'keras',
-	  'superjson',
-	  'wrapt',
-	  'matplotlib'
+    name="experiment_manager",
+    version="0.0.1",
+    author="Victor Ruelle",
+    author_email="ruelle.victor@gmail.com",
+    description="A thoughtless and all-round experiment manager for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/victorruelle/ExperimentManager",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'tensorflow',
+          'keras',
+          'superjson',
+          'wrapt',
+          'matplotlib',
+          'numpy'
       ],
-     long_description_content_type="text/markdown",
-     url="https://github.com/victorruelle/ExperimentManager",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Development Status :: Programming Language :: Python :: 3",
-         "License :: MIT License",
-         "Experiment :: Manager",
-     ],
-     include_package_data=True
- )
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Experiment :: Manager :: OS Independent",
+    ],
+)
